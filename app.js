cef16c8b3609d81b5ccfac23317a7973de6c849b
krish11189 <52022386+krish11189@users.noreply.github.com>
2021-10-27 16:02:08 -0500

    bonus

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var mydataRouter = require('./routes/mydata');
 
+var computationRouter = require('./routes/computation');
+
 var app = express();
 
 // view engine setup
@@ -25,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/mydata', mydataRouter);
+app.use('/computation', computationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
